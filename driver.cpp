1466754321f28d65e5b6e9a42f755c39607d953c
Mustafa Rashid <mustafa.rashidZA@gmail.com>
2017-04-24 14:34:33 +0200

    Adjust to support only one operation at a time

--- driver.cpp
+++ driver.cpp
@@ -18,50 +18,70 @@ int main (int argc, char* argv[]){
         RSHMUS001::VolImage vol;
 
         bool success = vol.readImages(string(argv[1])); //argv[1] is the image base
-        //vol.extract(5, "output");
 
         if (!success) {
-            cerr << "An error occured. Please check that your <imageBase> is correct" << endl;
+            cerr << "An error occured.\n Please check that your <imageBase> is correct\nPlease check that the folder name is correct" << endl;
             return 1;
         }
 
         cout << "Number of images: " << vol.volImageNum() << endl;
         cout << "Number of bytes required: " << vol.volImageSize() << endl;
 
-        for (int i = 2; i < argc; i++){
+        if (argc == 2){
+            return 1;
+        }
 
-            if (string(argv[i]) == "-d"){
+        string operation = string(argv[2]);
 
-                if ((i==2 && argc<6) || (i==6 && argc<9)) {
-                    cerr << "Incorrect number of arguments" << endl;
-                    return 1;
-                }
+        if (operation == "-d") {
 
-                int image1, image2;
-                istringstream(argv[i+1]) >> image1;
-                istringstream(argv[i+2]) >> image2;
-                vol.diffmap(image1, image2, argv[i+3]);
-                cout << "Computed a difference map between images " << image1 << "and" << image2 << "and wrote this out to file " << argv[i+3] << ".raw" << endl;
+            if (argc!=6) {
+                cerr << "Incorrect number of arguments" << endl;
+                return 1;
             }
 
-            if (string(argv[i]) == "-x"){
 
-                if ((i==2 && argc<5) || (i==6 && argc<8)) {
-                    cerr << "Incorrect number of arguments" << endl;
-                    return 1;
-                }
-
-                int sliceId;
-                istringstream(argv[i+1]) >> sliceId;
-                cout << sliceId;
-                vol.extract (sliceId, argv[i+2]);
-                cout << "Extracted the slice with number " << sliceId << " and wrote this to file "<< argv[i+2] << ".raw" << endl;
-
-            }
+            int image1, image2;
+            istringstream(argv[3]) >> image1;
+            istringstream(argv[4]) >> image2;
+            vol.diffmap(image1, image2, string(argv[5]));
+            cout << "Computed a difference map between images " << image1 << "and " << image2 << ", and wrote this out to file " << argv[5] << ".raw" << endl;
+            cout << "Generated header file, " << string(argv[5]) << " .data" << endl;
 
         }
 
+        if (operation == "-x"){
+
+            if (argc!=5) {
+                cerr << "Incorrect number of arguments" << endl;
+                return 1;
+            }
+
+            int sliceId;
+            istringstream(argv[3]) >> sliceId;
+            //cout << sliceId;
+            vol.extract (sliceId, string(argv[4]));
+            cout << "Extracted the slice with number " << sliceId << " and wrote this to file "<< argv[4] << ".raw" << endl;
+            cout << "Generated header file, " << string(argv[4]) << " .data" << endl;
+
+        }
+
+        if (operation == "-g"){
+
+            if (argc!=5) {
+                cerr << "Incorrect number of arguments" << endl;
+                return 1;
+            }
+
+            int rowId;
+            istringstream(argv[3]) >> rowId;
+            vol.sliceThrough(rowId, string(argv[4]));
+            cout << "Extracted an image along row " << rowId << " of the volume, across all slices. Output written to file " << argv[4] << ".raw" << endl;
+            cout << "Generated header file, " << string(argv[4]) << " .data" << endl;
+
+        }
     }
 
-    return 1;
+return 1;
+
 }
