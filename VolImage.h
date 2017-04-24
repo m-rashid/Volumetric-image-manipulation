03e186bcb6abfce88cbef891ca599ea4eec68744
Mustafa Rashid <mustafa.rashidZA@gmail.com>
2017-04-24 11:35:23 +0200

    Declare generateHeader(), volImageNum(), sliceThrough()

--- VolImage.h
+++ VolImage.h
@@ -29,9 +29,20 @@ namespace RSHMUS001 {
         // extract slice sliceId and write to output - define in .cpp
         void extract(int sliceId, string output_prefix);
 
+        // extract image along rowId of the volume, across all rows.
+      	//write output to file output_prefix.raw. Define in .cpp
+      	void sliceThrough(int rowId, string output_prefix);
+
         // number of bytes uses to store image data bytes
         //and pointers (ignore vector<> container, dims etc)
         int volImageSize(void); // define in .cpp
+
+        //number of images
+        //define in .cpp
+        int volImageNum(void);
+
+        //generate header file for each opearation
+        void generateHeader(int w, int h, int n, string output_prefix);
 
     };
 
